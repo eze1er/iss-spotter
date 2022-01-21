3237e7c5b5b5753be48d9fe0312ca0131cb67af3
eze1er <papaezechiel@gmail.com>
2022-01-21 00:03:07 +0000

    fly over time ok

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,12 +9,25 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
-
-fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+fetchCoordsByIP('70.48.199.230', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+
+// const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+const exampleCoords = { latitude: '43.693', longitude: '-79.8365' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
